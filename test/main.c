5360df40d1987e94875cd2fc18258427619b50fe
Sai Tarra <vardhan2131@gmail.com>
2025-07-31 09:03:52 +0530

    Added testing macros

--- test/main.c
+++ test/main.c
@@ -1,13 +1,56 @@
 #include "../include/cRegex.h"
 #include <stdio.h>
+#include <string.h>
+
+static int _passed;
+static int _total;
+
+#define TEST(A, B) do{\
+                        _total++; \
+                        if(A == B){_passed++;} \
+                        else{printf("Failed: %s != %s\n", #A, #B);}\
+                    }while(0);
+
+
 
 void terminalTesting();
+void automaticTesting();
 
-int main(){
+int main(int argc, char *argv[]){
 
-    terminalTesting();
-    return 0;
+    if(argc != 2){
+        printf("Usage: ./test <manual / automatic>\n");
+        return 0;
+    }
 
+    if(!strcmp(argv[1], "automatic")){
+        automaticTesting();
+    }
+    else if(!strcmp(argv[1], "manual")){
+        terminalTesting();
+    }
+    else{
+        printf("Usage: ./test <manual / automatic>\n");
+    }
+
+    if(_passed == _total){
+        return 0;
+    }
+    return -1;
+}
+
+
+void basicFullMatchTests(){
+
+    TEST(quickFullMatch("abc", "abc"), 1);
+    TEST(quickFullMatch("abc*", "abcc"), 1);
+    TEST(quickFullMatch("abc*", "ab"), 1);
+    TEST(quickFullMatch("a|b", "ab"), 0);
+}
+
+void automaticTesting(){
+    basicFullMatchTests();
+    printf("Passed: %d out of %d\n", _passed, _total);
 }
 
 void terminalTesting(){
@@ -39,7 +82,7 @@ void terminalTesting(){
         else if(type == '2' && 0 /*to do*/){
             
         }else{
-            printf("No Match\n");
+            printf("No Match\n\n");
         }
     }
 
